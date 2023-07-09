fac42f882f67899668ba72e93efe4999f19cb5b1
unknown <ebrahimamra69@gmail.com>
2023-07-09 14:36:50 +0300

    add note functionality

--- controllers/notes.controllers.ts
+++ controllers/notes.controllers.ts
@@ -21,7 +21,25 @@ export const getNotes = async (req: Request, res: Response) => {
 
 export const getNotesWithOwners = (req: Request, res: Response) => {};
 
-export const addNote = (req: Request, res: Response) => {};
+export const addNote = async (req: Request, res: Response) => {
+	try {
+		const note = req.body;
+		const response = await noteModel.create(note);
+
+		res.status(200).json({
+			success: true,
+			message: 'Notes added successfully',
+			data: response,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			success: false,
+			message: 'Error adding notes',
+			error: err,
+		});
+	}
+};
 
 export const deleteNote = (req: Request, res: Response) => {};
 
